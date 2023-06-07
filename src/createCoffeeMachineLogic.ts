1b18f8316d0eb2d8d68b880f92da90ee985c3043
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-06-07 09:34:55 +0200

    add Orange Juice

--- src/createCoffeeMachineLogic.ts
+++ src/createCoffeeMachineLogic.ts
@@ -1,9 +1,16 @@
-import { Drink, DrinkOrder, NumberOfSugars } from "./types";
+import type {
+  Drink,
+  HotDrink,
+  DrinkOrder,
+  NumberOfSugars,
+  Juice,
+} from "./types";
 
 type CoffeeMachineLogic = {
   makeCoffee: (nbOfSuggar?: NumberOfSugars) => DrinkOrder;
   makeTea: (nbOfSuggar?: NumberOfSugars) => DrinkOrder;
   makeChocolate: (nbOfSuggar?: NumberOfSugars) => DrinkOrder;
+  makeOrangeJuice: () => DrinkOrder;
   sendMessage: (message: string) => DrinkOrder;
   insertMoney: (money: number) => void;
 };
@@ -14,25 +21,40 @@ const price: Price = {
   T: 0.4,
   H: 0.5,
   C: 0.6,
+  O: 0.6,
 };
 export function createCoffeeMachineLogic(): CoffeeMachineLogic {
   let localMoney = 0;
 
-  const isEnoughMoney = (d: Drink): boolean =>
-    localMoney <= 0 || localMoney >= price[d];
-
-  const makeDrink = (drink: Drink, nbOfSuggar?: NumberOfSugars): DrinkOrder =>
-    isEnoughMoney(drink)
-      ? `${drink}:${nbOfSuggar ?? ""}:${nbOfSuggar ? 0 : ""}`
-      : sendMessage(`You need to provide ${price[drink] - localMoney} €`);
+  const makeDrink = (
+    drink: HotDrink,
+    nbOfSuggar?: NumberOfSugars
+  ): DrinkOrder => {
+    const drinkPrice = price[drink] - localMoney;
+    if (drinkPrice > 0) {
+      return sendMessage(`You need to provide ${price[drink] - localMoney} €`);
+    }
+    localMoney -= drinkPrice;
+    return `${drink}:${nbOfSuggar ?? ""}:${nbOfSuggar ? 0 : ""}`;
+  };
 
   const sendMessage = (message: string): DrinkOrder => `M:${message}`;
+
+  const makeJuice = (juice: Juice): DrinkOrder => {
+    const drinkPrice = price[juice] - localMoney;
+    if (drinkPrice > 0) {
+      return sendMessage(`You need to provide ${price[juice] - localMoney} €`);
+    }
+    localMoney -= drinkPrice;
+    return `${juice}::`;
+  };
 
   return {
     makeCoffee: (nbOfSuggar) => makeDrink("C", nbOfSuggar),
     makeTea: (nbOfSuggar) => makeDrink("T", nbOfSuggar),
     makeChocolate: (nbOfSuggar) => makeDrink("H", nbOfSuggar),
     sendMessage,
+    makeOrangeJuice: () => makeJuice("O"),
     insertMoney: (money) => (localMoney += money),
   };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,17 @@
-export type Drink = "T" | "C" | "H";
-
 export type NumberOfSugars = 1 | 2;
 
 export type Stick = 0 | "";
 
-export type DrinkOrder =
-  | `${Drink}:${NumberOfSugars | ""}:${Stick}`
-  | `M:${string}`;
+type Hot = "T" | "C" | "H";
+type ExtraHot = `${Hot}h`;
+
+export type Juice = "O";
+
+export type HotDrink = Hot;
+
+export type Drink = HotDrink | Juice;
+
+type JuiceOrder = `${Juice}::`;
+type HotDrinkOrder = `${HotDrink}:${NumberOfSugars | ""}:${Stick}`;
+
+export type DrinkOrder = HotDrinkOrder | JuiceOrder | `M:${string}`;
